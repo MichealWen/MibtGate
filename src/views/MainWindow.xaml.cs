f83ef2a9c19eb4b85bc28a06de98098dd565ca04
wenjia <micheal_wen@outlook.com>
2019-12-11 11:56:46 +0800

    [bug]:门限textbox显示错误

--- src/views/MainWindow.xaml.cs
+++ src/views/MainWindow.xaml.cs
@@ -144,4 +144,26 @@ namespace MbitGate.views
             throw new NotImplementedException();
         }
     }
+
+    public class CheckedMultiVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(values.Length > 1)
+            {
+                bool value1 = (bool)values[0];
+                bool value2 = (bool)values[1];
+                if(value1 && value2)
+                {
+                    return Visibility.Visible;
+                }
+            }
+            return Visibility.Hidden;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
